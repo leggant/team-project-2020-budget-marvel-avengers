f3691f47e6b7409e39a310ebedd6d893cd548f9b
Anthony Legg <leggtc1@student.op.ac.nz>
2021-03-19 01:26:42 +1300

    Evidence Form Fields Added
    Form fields to add student evidence have been created. Extra fields included to allow the user to search for the student that they are uploading records for. SASS/CSS files created to arrange the form fields. HTML structure needs some refining so fields are enclosed in a form tag

--- studio3project/resources/views/pages/evidence/evidence_upload.blade.php
+++ studio3project/resources/views/pages/evidence/evidence_upload.blade.php
@@ -5,54 +5,66 @@ Studio Management Portal | Upload Evidence
 @endsection
 
 @section('content')
-    <section class="container" id="evidence">
-        <h1>Evidence Uploads</h1>
-        <div class="form-wrapper">
-            <h2>Student Search</h2>
-            <form class="evidence-form">
-                <div class="input-group evidence-select-search-group">
-                    <div class="input-group-prepend">
-                        <p class="input-group-text">Search Student by:</p>
-                    </div>
-                    <select class="custom-select" id="inputGroupSelect">
-                        <option selected>Choose...</option>
-                        <option value="ID">ID#</option>
-                        <option value="Name">Name</option>
-                        <option value="Paper">Paper</option>
-                    </select>
-                    <input type="search" class="form-control" name="searchStudent" id="searchStudent" placeholder="Enter Here....">
-                    <button type="submit" class="form-control btn btn-outline-primary search-btn">Search</button>
-                </div>
-            </form>
+<setion class="grid-container" id="evidence">
+  <div class="title">
+      <h1>Evidence Uploads</h1>
+  </div>
+  <div class="search-output">
+  <h3>Select Student From Search Results</h3>
+  <div class="student-profile">
+        <label for="studentname1">Student Name 1
+        <input type="radio" checked name="test" value="studentid1" id="studentname1">
+        <img src="https://placeimg.com/50/50/tech">
+        </label>
+    </div>
+    <div class="student-profile">
+        <label for="studentname2">Student Name 2
+        <input type="radio" name="test" value="studentid2" id="studentname2">
+        <img src="https://placeimg.com/50/50/people">
+        </label>
+    </div>
+  </div>
+  <div class="form-search">
+  <h2>Student Search</h2>
+    <form class="evidence-form">
+        <div class="evidence-select-search-group">
+            <div class="input-group">
+                <p class="input-group-text">Search Student by:</p>
+            </div>
+            <select class="custom-select" id="inputGroupSelect">
+                <option selected>Choose...</option>
+                <option value="ID">ID#</option>
+                <option value="Name">Name</option>
+                <option value="Paper">Paper</option>
+            </select>
+            <input type="search" class="form-control" name="searchStudent" id="searchStudent" placeholder="Enter Here....">
+            <button type="submit" class="form-control btn btn-outline-primary search-btn">Search</button>
         </div>
-        <div>
-        <h2>Select Student From Search Results</h2>
-            <form class="evidence-form">
-                <div class="input-group mb-3">
-                    <div class="input-group-prepend">
-                        <p class="input-group-text">Search Student by:</p>
-                    </div>
-                    <select class="custom-select" id="inputGroupSelect">
-                        <option selected>Choose...</option>
-                        <option value="1">ID#</option>
-                        <option value="2">Name</option>
-                    </select>
-                    <input type="search" class="form-control" name="searchStudent" id="searchStudent">
-                    <button type="submit" class="form-control btn btn-outline-primary">Search</button>
-                </div>
-            </form>
+    </form>
+  </div>
+  <div class="file-upload">
+      <h2>Attach Evidence Documents</h2>
+    <form>
+        <div class="input-group">
+            <label for="textbox">
+            <p class="input-group-text">Additional Comments:</p>
+                <textarea name="comments" id="textbox" cols="30" rows="5" ></textarea>
+            </label>
         </div>
-        <div>
-            {{-- // set number of files to upload --}}
-            {{-- // set file types .ie PDF, Word, Jpeg, PNG, txt.... --}}
-            <form class="col-8">
-                <div class="input-group mb-3">
-                    <label class="input-group-text" for="inputFile">Upload</label>
-                    <input type="file" class="form-control" id="inputFile" multiple accept=".jpg,.jpeg,.png,.pdf,.doc,.docx,.html,.md">
-                    <input type="url" class="form-control" placeholder="Insert Weblink.....">
-                </div>
-                <button type="submit">Save Files</button>
-            </form>
+        <div class="input-group">
+            <label class="input-group-text" for="inputFile">Select File(s) For Upload</label>
+            <input type="file" class="form-control" id="inputFile" multiple accept=".jpg,.jpeg,.png,.pdf,.doc,.docx,.html,.md">
         </div>
-    </section>
+        <div class="input-group">
+            <label class="input-group-text" for="fileurl">Docuement URL</label>
+            <input type="url" id="fileurl" class="form-control" placeholder="Insert Weblink.....">
+        </div>
+        <div class="input-group">
+            <label for="DateRecieved">Date :</label>
+            <input type="datetime-local" id="DateRecieved" name="DateRecieved">
+        </div>
+        <button type="submit">Save Files</button>
+    </form>
+  </div>
+</setion>
 @endsection
