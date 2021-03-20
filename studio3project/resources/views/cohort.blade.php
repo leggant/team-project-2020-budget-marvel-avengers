3a2fbd54592c94e94716e2840e16d1d89fc32924
westcl4 <westcl4@D105A-68884>
2021-03-20 16:38:59 +1300

    Finished basic form structure

--- studio3project/resources/views/cohort.blade.php
+++ studio3project/resources/views/cohort.blade.php
@@ -16,88 +16,50 @@
         </header>
         
         <div>
-          <label class="desc" id="title1" for="Field1">Full Name</label>
-          <div>
-            <input id="Field1" name="Field1" type="text" class="field text fn" value="" size="8" tabindex="1">
-          </div>
-        </div>
-          
-        <div>
-          <label class="desc" id="title3" for="Field3">
-            Email
-          </label>
-          <div>
-            <input id="Field3" name="Field3" type="email" spellcheck="false" value="" maxlength="255" tabindex="3"> 
-         </div>
-        </div>
-          
-        <div>
-          <label class="desc" id="title4" for="Field4">
-            Message
-          </label>
-        
-          <div>
-            <textarea id="Field4" name="Field4" spellcheck="true" rows="10" cols="50" tabindex="4"></textarea>
-          </div>
-        </div>
-          
-        <div>
           <fieldset>
           
             <legend id="title5" class="desc">
-              Select a Choice
+              Select a Cohort
             </legend>
             
             <div>
                 <input id="radioDefault_5" name="Field5" type="hidden" value="">
                 <div>
                     <input id="Field5_0" name="Field5" type="radio" value="First Choice" tabindex="5" checked="checked">
-                    <label class="choice" for="Field5_0">First Choice</label>
+                    <label class="choice" for="Field5_0">Cohort 1</label>
                 </div>
               <div>
                   <input id="Field5_1" name="Field5" type="radio" value="Second Choice" tabindex="6">
-                  <label class="choice" for="Field5_1">Second Choice</label>
+                  <label class="choice" for="Field5_1">Cohort 2</label>
               </div>
               <div>
                   <input id="Field5_2" name="Field5" type="radio" value="Third Choice" tabindex="7">
-                  <label class="choice" for="Field5_2">Third Choice</label>
+                  <label class="choice" for="Field5_2">Cohort 3</label>
               </div>
             </div>
           </fieldset>
         </div>
-        
-        <div>
-          <fieldset>
-            <legend id="title6" class="desc">
-              Check All That Apply
-            </legend>
-            <div>
-            <div>
-                <input id="Field6" name="Field6" type="checkbox" value="First Choice" tabindex="8">
-                <label class="choice" for="Field6">First Choice</label>
-            </div>
-            <div>
-                <input id="Field7" name="Field7" type="checkbox" value="Second Choice" tabindex="9">
-                <label class="choice" for="Field7">Second Choice</label>
-            </div>
-            <div>
-                <input id="Field8" name="Field8" type="checkbox" value="Third Choice" tabindex="10">
-                <label class="choice" for="Field8">Third Choice</label>
-            </span>
-            </div>
-          </fieldset>
-        </div>
-        
+          
         <div>
           <label class="desc" id="title106" for="Field106">
-              Select a Choice
+              Select Students
           </label>
           <div>
           <select id="Field106" name="Field106" class="field select medium" tabindex="11"> 
-            <option value="First Choice">First Choice</option>
-            <option value="Second Choice">Second Choice</option>
-            <option value="Third Choice">Third Choice</option>
+            <option value="First Choice">Joe Bloggs</option>
+            <option value="Second Choice">Mary Jane</option>
+            <option value="Third Choice">Donald Trump</option>
           </select>
+          </div>
+        </div>
+
+        <div>
+          <label class="desc" id="title4" for="Field4">
+            Selected Students
+          </label>
+        
+          <div>
+            <textarea id="Field4" name="Field4" spellcheck="true" rows="10" cols="50" tabindex="4"></textarea>
           </div>
         </div>
         
