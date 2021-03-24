736231349936e964607d224570cb508a5d45b82e
Hayden <mcdohr2@student.op.ac.nz>
2021-03-24 23:23:56 +1300

    changed layout and added more student information

--- studio3project/resources/views/Notes.blade.php
+++ studio3project/resources/views/Notes.blade.php
@@ -1,32 +1,34 @@
 <form action="#" id="js-form">
-  <div class="name">
+
+  <div class="student-info">
+  <label for="notesBox">First Name:</label>
     <input type="text" name="name" placeholder="Adam" />
+    <label for="notesBox">Family Name:</label>
     <input type="text" name="surname" placeholder="Smith" />
-    <input type="mail" name="email" placeholder="adam.smith@mail.com"/>
-  </div>
-  <div class="radio">
-    <label for="radio-choice-1">Choice 1</label>
-    <input type="radio" name="radio-choice" id="radio-choice-1" value="choice-1" />
-    <label for="radio-choice-2">Choice 2</label>
-    <input type="radio" name="radio-choice" id="radio-choice-2" value="choice-2" />
-  </div>
-  <div class="dropdown">
-    <label for="select-choice">Select Dropdown Choice:</label>
+    <label for="notesBox">Student ID:</label>
+    <input type="integer" name="studentID" placeholder="20007367"/>
+    <br>
+
+    <div class="class-dropdown">
+    <label for="select-choice">Studio Class:</label>
     <select name="select-choice" id="select-choice">
-      <option value="Choice 1">Choice 1</option>
-      <option value="Choice 2">Choice 2</option>
-      <option value="Choice 3">Choice 3</option>
+      <option value="Choice 1">Studio 3</option>
+      <option value="Choice 2">Studio 4</option>
+      <option value="Choice 3">Studio 5</option>
+      <option value="Choice 3">Studio 6</option>
     </select>
+    <label for="notesBox">Year: </label>
+    <input type="integer" name="Year" placeholder="2021"/>
   </div>
+
+  </div>
+
   <div class="message">
-    <label for="textarea">Textarea:</label>
-    <textarea cols="40" rows="8" name="textarea" id="textarea"></textarea>
+    <label for="notesBox">Add Student Notes:</label>
+    <textarea cols="50" rows="15" name="notesBox" id="notesBox"></textarea>
   </div>
-  <div class="check">
-    <label for="checkbox">Checkbox:</label>
-    <input type="checkbox" name="checkbox" id="checkbox" />
-  </div>
-  <div class="submit">
+
+  <div class="submit-btn">
     <input type="submit" value="Submit" disabled/>
   </div>
 </form>
