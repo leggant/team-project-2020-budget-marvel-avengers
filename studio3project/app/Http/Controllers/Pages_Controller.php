c4e5bf05ea27b923d300bd7b55dd346d65825339
Conor West <westcl4@student.op.ac.nz>
2021-03-25 22:19:42 +1300

    Added copy controller

---
+++ studio3project/app/Http/Controllers/Pages_Controller.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+//use  App\Models\Cohort;
+
+class PagesController extends Controller
+{
+    public function cohortpage(){
+    return view('pages.cohort');
+    }
+}
