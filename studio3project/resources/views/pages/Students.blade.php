552f775660a56c452a5411680fad17cd0419f0b3
Josephhongg <joseph.hong99@gmail.com>
2021-03-26 16:36:19 +1300

    added html

--- studio3project/resources/views/pages/Students.blade.php
+++ studio3project/resources/views/pages/Students.blade.php
@@ -1,4 +1,31 @@
-@extends('layouts.app')
-@section('homepage-content')
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Student Page</title>
+    <link rel="stylesheet" href="styles.css">
+</head>
+<body>
+    <h1>Student Page</h1>
+    <form action="">
+        <label for="fname">First name:</label><br>
+        <input type="text" id="fname" name="fname"><br><br>
+        <label for="lname">Last name:</label><br>
+        <input type="text" id="lname" name="lname"><br><br>
+        <label for="ghUser">GitHub Username:</label><br>
+        <input type="text" id="ghUser" name="ghUser"><br><br>
+        <label for="idNumber">Student ID Number:</label><br>
+        <input type="number" id="idNumber" name="idNumber"><br><br>
+        <input type="radio" id="male" name="gender" value="male">
+        <label for="male">Male</label><br>
+        <input type="radio" id="female" name="gender" value="female">
+        <label for="female">Female</label><br><br>
+        
+        <input type="submit" value="Submit">
 
-    @endsection
+        
+    </form>
+</body>
+</html>
