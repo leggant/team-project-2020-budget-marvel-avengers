cad5180263b1e5536b46d1728f8e4cd7b1ae75f5
Anthony Legg <leggtc1@student.op.ac.nz>
2021-03-29 15:13:22 +1300

    fixing evidence screeen
    moved project out of studio3project directory

--- routes/web.php
+++ routes/web.php
@@ -8,6 +8,5 @@ Route::get('/', [PagesController::class, 'homepage'])->name('pages.index');
 Route::get('/evidence-upload', [PagesController::class, 'EvidencePage'])->name('pages.evidence');
 Route::get('/home',[PagesController::class ,'homepage']);
 Route::get('/students',[PagesController::class ,'students']);
-Route::get('/evidence',[PagesController::class ,'evidence']);
 Route::get('/notes',[PagesController::class ,'notes']);
 Route::get('/about',[PagesController::class ,'About']);
