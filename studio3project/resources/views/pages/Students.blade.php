16dbf66e46bdc32189799de16ae1cdb1f9a25738
Josephhongg <joseph.hong99@gmail.com>
2021-03-30 14:36:31 +1300

    updated html on student page

--- studio3project/resources/views/pages/Students.blade.php
+++ studio3project/resources/views/pages/Students.blade.php
@@ -1,14 +1,6 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Student Page</title>
-    <link rel="stylesheet" href="styles.css">
-</head>
-<body>
-    <h1>Student Page</h1>
+@extends('layouts.app')
+@section('homepage-content')
+<h1>Student Page</h1>
     <form action="">
         <label for="fname">First name:</label><br>
         <input type="text" id="fname" name="fname"><br><br>
@@ -29,5 +21,4 @@
 
         
     </form>
-</body>
-</html>
+@endsection
