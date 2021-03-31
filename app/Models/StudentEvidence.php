890c1d3b84322a2572d8167d486d700027b596a7
Anthony Legg <leggtc1@student.op.ac.nz>
2021-03-31 14:25:55 +1300

    draft student evidence migrate and model ready for code review
    some fields in the migration have been commented out as these lines of code are not needed in this sprint

--- app/Models/StudentEvidence.php
+++ app/Models/StudentEvidence.php
@@ -9,4 +9,10 @@ class StudentEvidence extends Model
 {
     use HasFactory;
     protected $table = 'student_evidence';
+    protected $primaryKey = 'evidence_id';
+    protected $fillable = ['student_id', 'staff_id', 'cohort_id', 'file_uploads', 'comments'];
+    protected $casts = [
+        'file_uploads' => 'array',
+        'comments' => 'array'
+    ];
 }
--- database/migrations/2021_03_29_075317_create_student_evidence_table.php
+++ database/migrations/2021_03_29_075317_create_student_evidence_table.php
@@ -3,6 +3,7 @@
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
+use App\Models\StudentEvidence;
 
 class CreateStudentEvidenceTable extends Migration
 {
@@ -14,11 +15,25 @@ class CreateStudentEvidenceTable extends Migration
     public function up()
     {
         Schema::create('student_evidence', function (Blueprint $table) {
-            $table->increments('id');
-            $table->integer('student_id')->unique();
-            $table->timestamps('uploaded_at');
-            $table->timestamps('updated_at');
-            //$table->timestamps();
+            $table->bigIncrements('evidence_id');
+            $table->unsignedInteger('student_id')->nullable(false)->unique();
+            $table->unsignedInteger('staff_id')->nullable(false)->unique();
+            $table->unsignedInteger('cohort_id')->nullable(false)->unique();
+            
+            // shorthand way of adding foreign key, only works if a table 
+            // migration exists-which cohorts currently does. Will uncomment this when
+            // the foreign keys exist in other table/migrations
+            // $table->foreignId('student_id')->constrained('students');  
+            // $table->foreignId('staff_id')->constrained('staff');  
+            // $table->foreignId('cohort_id')->constrained('cohorts');  
+            
+            // laravel cannot create arrays natively, the recommended 
+            // workaround is to use json in the migration
+            // in the model this json is cast to an array which is used in/by the
+            // database 
+            $table->json('file_uploads');
+            $table->json('comments');
+            $table->timestamps();
         });
     }
 
