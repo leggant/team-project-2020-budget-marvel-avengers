a07025ba5e485764e50a7874fb03e12cf069500a
Mohammed-alhasan <chookepsn@gmail.com>
2021-03-31 19:50:47 +1300

    added model and migration

---
+++ database/migrations/2021_03_31_061910_create_student_screens_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateStudentScreensTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('student_screens', function (Blueprint $table) {
+            $table->id('student_id');
+            $table->string('first_name');
+            $table->string('last_name');
+            $table->string('github_username');
+            $table->string('email');
+            $table->string('gender');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('student_screens');
+    }
+}
