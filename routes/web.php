66562d1d09cd8f11ceb6e97faf73fe764a0b45ef
Mohammed Alhasan <50258432+Mohammed-alhasan@users.noreply.github.com>
2021-04-01 10:56:15 +1300

    prevent possible conflict issue

--- routes/web.php
+++ routes/web.php
@@ -6,5 +6,7 @@ use App\Http\Controllers\PagesController;
 Route::get('/', [PagesController::class, 'homepage'])->name('pages.index');
 Route::get('/cohortAdmin', [PagesController::class , 'cohortpage']);
 Route::get('/evidence-upload', [PagesController::class, 'EvidencePage'])->name('pages.evidence');
-Route::get('/students',[PagesController::class ,'students'])->name('pages.students');
 Route::get('/notes',[PagesController::class ,'notes']);
+Route::get('/students',[PagesController::class ,'students'])->name('pages.students');
+
+ 
