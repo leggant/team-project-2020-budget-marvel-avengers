e852759158949ccf83a4d9adedcadd2e63246587
Anthony Legg <leggtc1@student.op.ac.nz>
2021-04-03 20:41:29 +1300

    gitignore file correction 2
    also tidyed up code within the model file for easier reading

--- app/Models/StudentEvidence.php
+++ app/Models/StudentEvidence.php
@@ -11,7 +11,20 @@ class StudentEvidence extends Model
     protected $table = 'student_evidence';
     protected $primaryKey = 'evidence_id';
     protected $dateFormat = 'Y-m-d H:i';
-    protected $fillable = ['student_id', 'staff_id', 'cohort_id', 'file_uploads', 'comments', 'urls', 'date_received', 'assignment_received', 'assignment_due', 'medical_certificate_supplied', 'medical_cert_start', 'medical_cert_end'];
+    protected $fillable = [
+        'student_id', 
+        'staff_id', 
+        'cohort_id', 
+        'file_uploads', 
+        'comments', 
+        'urls', 
+        'date_received', 
+        'assignment_received', 
+        'assignment_due', 
+        'medical_certificate_supplied', 
+        'medical_cert_start', 
+        'medical_cert_end'
+    ];
     protected $casts = [
         'file_uploads' => 'array',
         'comments' => 'array',
