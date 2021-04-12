8747d97fc7503f6e4b1ca914e72ec3009ec08929
Anthony Legg <leggtc1@student.op.ac.nz>
2021-04-12 14:58:52 +1200

    update made to form input field data types

--- resources/views/pages/students/students.blade.php
+++ resources/views/pages/students/students.blade.php
@@ -1,19 +1,19 @@
 @extends('layouts.app')
 @section('content')
-<section>
-    <h1>Student Page</h1>
-    <form action="">
-        <label for="fname">First name:</label>
-        <input type="text" id="fname" name="fname">
-        <label for="lname">Last name:</label>
-        <input type="text" id="lname" name="lname">
-        <label for="ghUser">GitHub Username:</label>
-        <input type="text" id="ghUser" name="ghUser">
-        <label for="idNumber">Student ID Number:</label>
-        <input type="number" id="idNumber" name="idNumber">
-        <label for="email">Student Email:</label>
-        <input type="text" name="email" id="email">
-        <input type="submit" value="Submit" class="submit-btn">
-    </form>
-</section>
+    <section>
+        <h1>Student Page</h1>
+        <form action="">
+            <label for="fname">First name:</label>
+            <input type="text" id="fname" name="fname">
+            <label for="lname">Last name:</label>
+            <input type="text" id="lname" name="lname">
+            <label for="ghUser">GitHub Username:</label>
+            <input type="text" id="ghUser" name="ghUser">
+            <label for="idNumber">Student ID Number:</label>
+            <input type="text" id="idNumber" name="idNumber">
+            <label for="email">Student Email:</label>
+            <input type="email" name="email" id="email">
+            <button class="submit-btn" type="submit">Submit</button>
+        </form>
+    </section>
 @endsection
