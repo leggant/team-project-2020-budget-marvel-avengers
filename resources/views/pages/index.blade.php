b31f0857b36e827c3797490ecc1674eabedea08d
Anthony Legg <leggtc1@student.op.ac.nz>
2021-04-12 13:28:22 +1200

    Updates to notes form html and css
    HTML had excessive wrapper divs and no form element wrapping form input fields

--- resources/views/pages/index.blade.php
+++ resources/views/pages/index.blade.php
@@ -4,5 +4,4 @@
     <h1>Welcome</h1>
     <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Repellendus magni illo ab voluptates possimus ipsum quo excepturi expedita ipsa? Nihil deleniti aut laudantium beatae odit exercitationem fuga impedit esse sit.</p>
 </div>
-
 @endsection
--- resources/views/pages/notes/Notes.blade.php
+++ resources/views/pages/notes/Notes.blade.php
@@ -6,53 +6,17 @@ Studio Management Portal | Notes
 
 @section('content')
 <section>
-
-<h1>Student Notes</h1>
-
-
-<div>
-  <label class="desc" id="First-name" for="First-name">
-    First Name
-  </label>
-
-  <div>
-    <input id="First-name" name="First-name" type="text" class="field text fn" value="" size="8" tabindex="1"  placeholder="First Name">
-  </div>
-</div>
-
-<div>   
-    <label class="desc" id="Last-name" for="Last-name">
-      Last Name
-    </label>
-    <div>
-
+  <h1>Student Notes</h1>
+  <form action="">
+    <label class="desc" for="First-name">First Name</label>
+    <input id="First-name" name="First-name" type="text" class="field text fn" value="" size="8" tabindex="1"  placeholder="First Name"> 
+    <label class="desc" for="Last-name">Last Name</label>
     <input id="Last-name" name="Last-name" type="text" class="field text fn" value="" size="8" tabindex="1" placeholder="Last Name"> 
-  </div>
-</div>
-
-<div>
-  <label class="desc" id="Student-id" for="Student-id">
-    Student ID:
-  </label>
-  <div>
+    <label class="desc" for="Student-id">Student ID:</label>
     <input id="Student-id" name="Student-id" type="email" spellcheck="false" value="" maxlength="255" tabindex="3" placeholder="ID Number"> 
- </div>
-</div>
-  
-<div>
-  <label class="desc" id="Notes" for="Notes">
-    Notes:
-  </label>
-
-  <div>
-    <textarea id="FielNotesd6" name="Notes" spellcheck="true" rows="10" cols="50" tabindex="4" placeholder="Enter notes here"></textarea>
-  </div>
-</div>
-
-<div>
-  <div>
+    <label class="desc" for="FileNotes">Notes:</label>
+    <textarea id="FileNotes" name="Notes" spellcheck="true" rows="10" cols="50" tabindex="4" placeholder="Enter notes here"></textarea>
     <input id="saveForm" name="saveForm" type="submit" value="Submit">
-  </div>
-</div>
+  </form>
 </section>
 @endsection
