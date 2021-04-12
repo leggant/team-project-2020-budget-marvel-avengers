13318655e4b26c07f2ba61b9f2ec10f1e13b4ca4
Anthony Legg <leggtc1@student.op.ac.nz>
2021-04-12 13:37:29 +1200

    update to student page form and form sass files
    removed <br> tags from html and commented out gender input elements as these are not required by the client

--- resources/views/pages/students/students.blade.php
+++ resources/views/pages/students/students.blade.php
@@ -3,20 +3,20 @@
 <section>
     <h1>Student Page</h1>
     <form action="">
-        <label for="fname">First name:</label><br>
-        <input type="text" id="fname" name="fname"><br><br>
-        <label for="lname">Last name:</label><br>
-        <input type="text" id="lname" name="lname"><br><br>
-        <label for="ghUser">GitHub Username:</label><br>
-        <input type="text" id="ghUser" name="ghUser"><br><br>
-        <label for="idNumber">Student ID Number:</label><br>
-        <input type="number" id="idNumber" name="idNumber"><br><br>
-        <label for="email">Student Email:</label><br>
-        <input type="text" name="email" id="email"><br><br>
-        <input type="radio" id="male" name="gender" value="male">
-        <label for="male">Male</label><br>
+        <label for="fname">First name:</label>
+        <input type="text" id="fname" name="fname">
+        <label for="lname">Last name:</label>
+        <input type="text" id="lname" name="lname">
+        <label for="ghUser">GitHub Username:</label>
+        <input type="text" id="ghUser" name="ghUser">
+        <label for="idNumber">Student ID Number:</label>
+        <input type="number" id="idNumber" name="idNumber">
+        <label for="email">Student Email:</label>
+        <input type="text" name="email" id="email">
+        <!-- <input type="radio" id="male" name="gender" value="male">
+        <label for="male">Male</label>
         <input type="radio" id="female" name="gender" value="female">
-        <label for="female">Female</label><br><br>
+        <label for="female">Female</label> -->
         <input type="submit" value="Submit">
     </form>
 </section>
