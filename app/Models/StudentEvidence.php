33b1264b58b37bb93183ba15fdf7b7deb5d05144
Anthony Legg <leggtc1@student.op.ac.nz>
2021-04-12 21:05:11 +1200

    Updates made to evidence model/migration naming conventions
    Changed all attributes names that had underscores and named them using camel-case

--- app/Models/StudentEvidence.php
+++ app/Models/StudentEvidence.php
@@ -8,22 +8,22 @@ use Illuminate\Database\Eloquent\Model;
 class StudentEvidence extends Model
 {
     use HasFactory;
-    protected $table = 'student_evidence';
-    protected $primaryKey = 'evidence_id';
+    protected $table = 'studentEvidence';
+    protected $primaryKey = 'id';
     protected $dateFormat = 'Y-m-d H:i';
     protected $fillable = [
-        'student_id', 
-        'staff_id', 
-        'cohort_id', 
-        'file_uploads', 
+        'studentId', 
+        'staffId', 
+        'cohortId', 
+        'fileUploads', 
         'comments', 
         'urls', 
-        'date_received', 
-        'assignment_received', 
-        'assignment_due', 
-        'medical_certificate_supplied', 
-        'medical_cert_start', 
-        'medical_cert_end'
+        'dateReceived', 
+        'assignmentReceived', 
+        'assignmentDue', 
+        'medicalCertificateSupplied', 
+        'medicalCertStart', 
+        'medicalCertEnd'
     ];
     protected $casts = [
         'file_uploads' => 'array',
--- database/migrations/2021_03_29_075317_create_student_evidence_table.php
+++ database/migrations/2021_03_29_075317_create_student_evidence_table.php
@@ -14,24 +14,24 @@ class CreateStudentEvidenceTable extends Migration
      */
     public function up()
     {
-        Schema::create('student_evidence', function (Blueprint $table) {
-            $table->bigIncrements('evidence_id');
-            $table->unsignedInteger('student_id')->nullable(false)->unique();
-            $table->unsignedInteger('staff_id')->nullable(false)->unique();
-            $table->unsignedInteger('cohort_id')->nullable(false)->unique();
+        Schema::create('studentEvidence', function (Blueprint $table) {
+            $table->id('id');
+            $table->unsignedInteger('studentId')->nullable(false)->unique();
+            $table->unsignedInteger('staffId')->nullable(false)->unique();
+            $table->unsignedInteger('cohortId')->nullable(false)->unique();
             // laravel cannot create arrays natively, the recommended 
             // workaround is to use json in the migration
             // in the model this json is cast to an array which is used in/by the
             // database 
-            $table->json('file_uploads')->nullable(true);
+            $table->json('fileUploads')->nullable(true);
             $table->json('comments');
             $table->json('urls')->nullable(true);
-            $table->datetime('date_received')->format('d.m.y h.m');
-            $table->datetime('assignment_received')->format('d.m.y h.m')->nullable(true);
-            $table->datetime('assignment_due')->format('d.m.y h.m')->nullable(true);
-            $table->boolean('medical_certificate_supplied')->default(false);
-            $table->date('medical_cert_start')->format('d.m.y')->nullable(true);
-            $table->date('medical_cert_end')->format('d.m.y')->nullable(true);
+            $table->datetime('dateReceived')->format('d.m.y h.m');
+            $table->datetime('assignmentReceived')->format('d.m.y h.m')->nullable(true);
+            $table->datetime('assignmentDue')->format('d.m.y h.m')->nullable(true);
+            $table->boolean('medicalCertificateSupplied')->default(false);
+            $table->date('medicalCertStart')->format('d.m.y')->nullable(true);
+            $table->date('medicalCertEnd')->format('d.m.y')->nullable(true);
             $table->timestamps();
         });
     }
