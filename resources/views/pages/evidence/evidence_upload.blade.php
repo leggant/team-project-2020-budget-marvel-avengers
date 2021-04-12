3c7dc92a0c5d962da6fe234ae1801f3e06d195a5
Anthony Legg <leggtc1@student.op.ac.nz>
2021-04-12 20:41:50 +1200

    Fixed evidence screen HTML missing closing tags

--- resources/views/pages/evidence/evidence_upload.blade.php
+++ resources/views/pages/evidence/evidence_upload.blade.php
@@ -5,34 +5,36 @@
 @endsection
 
 @section('content')
-    <section id="evidence">
-        <div class="section-title">
-            <h1>Evidence Uploads</h1>
-        </div>
-        <div class="file-upload">
-            <h2>Attach Student Evidence Documents</h2>
-            <form>
-                <div class="form-col">
-                    <div class="input-group">
-                        <label for="inputFile">Select File(s) For Upload</label>
-                        <input type="file" id="inputFile" multiple
-                            accept=".jpg,.jpeg,.png,.pdf,.doc,.docx,.html,.md,.zip,.7z,.rar">
-                    </div>
-                    <div class="input-group">
-                        <label for="textbox">Additional Comments: </label>
-                        <textarea name="additional-comments" id="additional-comments"></textarea>
-                    </div>
-                    <button class="submit-btn" type="submit">Save Files</button>
+<section id="evidence">
+    <div class="section-title">
+        <h1>Evidence Uploads</h1>
+    </div>
+    <div class="file-upload">
+        <h2>Attach Student Evidence Documents</h2>
+        <form>
+            <div class="form-col">
+                <div class="input-group">
+                    <label for="inputFile">Select File(s) For Upload</label>
+                    <input type="file" id="inputFile" multiple
+                        accept=".jpg,.jpeg,.png,.pdf,.doc,.docx,.html,.md,.zip,.7z,.rar">
                 </div>
-                <div class="form-col">
-                    <div class="input-group">
-                        <label for="fileurl">Document URL</label>
-                        <input type="url" id="fileurl" class="form-control" placeholder="Insert Weblink.....">
-                    </div>
-                    <div class="input-group">
-                        <label for="DateRecieved">Date Recieved: </label>
-                        <input type="datetime-local" id="DateRecieved" name="DateRecieved">
-                    </div>
+                <div class="input-group">
+                    <label for="textbox">Additional Comments: </label>
+                    <textarea name="additional-comments" id="additional-comments"></textarea>
                 </div>
-            </form>
+                <button class="submit-btn" type="submit">Save Files</button>
+            </div>
+            <div class="form-col">
+                <div class="input-group">
+                    <label for="fileurl">Document URL</label>
+                    <input type="url" id="fileurl" class="form-control" placeholder="Insert Weblink.....">
+                </div>
+                <div class="input-group">
+                    <label for="DateRecieved">Date Recieved: </label>
+                    <input type="datetime-local" id="DateRecieved" name="DateRecieved">
+                </div>
+            </div>
+        </form>
+    </div>
+</section>
 @endsection
