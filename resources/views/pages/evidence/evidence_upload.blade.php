74e3f8f1392589193c0532b697384edbd6953d84
westcl4 <westcl4@D105A-68884>
2021-04-12 15:00:17 +1200

    Formatted views using Prettier for Blade

--- resources/views/pages/evidence/evidence_upload.blade.php
+++ resources/views/pages/evidence/evidence_upload.blade.php
@@ -5,6 +5,7 @@
 @endsection
 
 @section('content')
+<<<<<<< HEAD
     <section id="evidence">
         <div class="section-title">
             <h1>Evidence Uploads</h1>
@@ -35,6 +36,36 @@
                     </div>
                 </div>
             </form>
+=======
+<section id="evidence">
+  <div class="section-title">
+      <h1>Evidence Uploads</h1>
+  </div>
+  <div class="file-upload">
+      <h2>Attach Student Evidence Documents</h2>
+    <form>
+        <div class="form-col">
+            <div class="input-group">
+                <label for="inputFile">Select File(s) For Upload</label>
+                <input type="file" id="inputFile" multiple accept=".jpg,.jpeg,.png,.pdf,.doc,.docx,.html,.md,.zip,.7z,.rar">
+            </div>
+            <div class="input-group">
+                <label for="textbox">Additional Comments: </label>
+                <textarea name="additional-comments" id="additional-comments"></textarea>
+            </div>
+            
+        </div>
+        <div class="form-col">
+            <div class="input-group">
+                <label for="fileurl">Document URL</label>
+                <input type="url" id="fileurl" class="form-control" placeholder="Insert Weblink.....">
+            </div>
+            <div class="input-group">
+                <label for="DateRecieved">Date Recieved: </label>
+                <input type="datetime-local" id="DateRecieved" name="DateRecieved">
+            </div>
+            <button class="submit-btn" type="submit">Save Files</button>
+>>>>>>> 3b8ab562aa727b2545e9c0e0faa8ee0b4b4e729f
         </div>
     </section>
 @endsection
