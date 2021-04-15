6ab54c5a3b4ef661e96db67f350d3b8c62948333
Hayden-McD <64762334+Hayden-McD@users.noreply.github.com>
2021-04-15 12:00:03 +1200

    Update Note.php

--- app/Models/Note.php
+++ app/Models/Note.php
@@ -8,7 +8,6 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 class Note extends Model
 {
     use HasFactory;
-
     protected $table = 'notes';
     protected $primaryKey = 'student_id';
     protected $fillable = ['student_id', 'first_name', 'last_name', 'repo_url', 'notes', 'group_id', 'group_name'];
