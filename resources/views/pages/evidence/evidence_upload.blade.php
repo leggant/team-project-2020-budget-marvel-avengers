341e6480e6901be42b0a56088fa3c2f38611a93f
Anthony Legg <leggtc1@student.op.ac.nz>
2021-04-29 22:13:49 +1200

    Update to evidence form html. This is now more closely aligned with the model and migration

--- resources/views/pages/evidence/evidence_upload.blade.php
+++ resources/views/pages/evidence/evidence_upload.blade.php
@@ -8,10 +8,10 @@
 <section id="evidence">
     <header>
         <h1>Evidence Uploads</h1>
-        <h2>Upload Student Evidence Documents</h2>
     </header>
     <form>
         <div class="form-col">
+            <h3>Find Student</h3>
             <div class="input-group">
                 <label for="studioPaper">Select Studio Paper:</label>
                 <select name="studioPapers" id="studioPaper">
@@ -25,25 +25,35 @@
             </div>
             <div class="input-group">
                 <label for="semester-1">Semester 1</label>
-                <input type="radio" name="semester" id="semester-1">
+                <input type="radio" name="semester" id="semester-1" value="semester-1" checked>
                 <label for="semester-2">Semester 2</label>
-                <input type="radio" name="semester" id="semester-2">
+                <input type="radio" name="semester" id="semester-2" value="semester-2">
             </div>
-            <label for="inputFile">Select File(s) For Upload</label>
             <div class="input-group">
-                <input type="file" id="inputFile" multiple
-                    accept=".jpg,.jpeg,.png,.pdf,.doc,.docx,.html,.md,.zip,.7z,.rar">
+                <label for="students">Select Student:</label>
+                <select name="studioStudents" id="students">
+                    <option value="student-1">Student One</option>
+                    <option value="student-2">Student Two</option>
+                    <option value="student-3">Student Three</option>
+                    <option value="student-4">Student Four</option>
+                    <option value="student-5">Student Five</option>
+                </select>
+            </div>
+            <div class="input-group">
+                <label for="fileurl">Document URL</label>
+                <input type="url" id="fileurl" class="form-control" placeholder="Insert Weblink.....">
             </div>
             <label for="textbox">Additional Comments: </label>
             <div class="input-group">
                 <textarea name="additional-comments" id="additional-comments"></textarea>
             </div>
-            <button class="submit-btn" type="submit">Save Files</button>
+            
         </div>
         <div class="form-col">
+            <h3>Upload Files</h3>
             <div class="input-group">
-                <label for="fileurl">Document URL</label>
-                <input type="url" id="fileurl" class="form-control" placeholder="Insert Weblink.....">
+                <input type="file" id="inputFile" multiple
+                    accept=".jpg,.jpeg,.png,.pdf,.doc,.docx,.html,.md,.zip,.7z,.rar">
             </div>
             <div class="input-group">
                 <label for="DateRecieved">Date Recieved: </label>
@@ -59,9 +69,14 @@
                 <input type="datetime-local" id="medicalEnd" name="medicalEnd">
             </div>
             <div class="input-group">
+                <input type="file" id="medicalFile"
+                    accept=".jpg,.jpeg,.png,.pdf">
+            </div>
+            <div class="input-group">
                 <label for="medicalCert">Medical Certificate Supplied: </label>
                 <input type="checkbox" id="medicalCert" name="medicalCert" value="true">
             </div>
+            <button class="submit-btn" type="submit">Save Files</button>
         </div>
     </form>
 </section>
