f3302656b227bffb5fc0ddf54dc2bbf3be44ad0b
Anthony Legg <leggtc1@student.op.ac.nz>
2021-04-30 12:47:28 +1200

    Fix homepage html bug

--- resources/views/pages/index.blade.php
+++ resources/views/pages/index.blade.php
@@ -1,11 +1,11 @@
 @extends('layouts.app')
 @section('content')
-<section
-<div class="homepageCarousel">
-  <div class="owl-carousel owl-theme mt-5">
-    <div class="item"><img src="https://www.op.ac.nz/assets/Campaign2020/Study-For-Free-Homepage-Banner-v2__FillWzk0OCw1MzRd.jpg" alt=""></div>
-    <div class="item"><img src="https://archipro.co.nz/assets/photos/Otago-Polytechnic-Forth-Street-Mason--Wales-Architects-16204__ListingLargeW10.JPG?1" alt=""></div>
-</div>
+<section>
+  <div class="homepageCarousel carousel">
+    <div class="owl-carousel owl-theme mt-5">
+      <div class="item"><img src="https://www.op.ac.nz/assets/Campaign2020/Study-For-Free-Homepage-Banner-v2__FillWzk0OCw1MzRd.jpg" alt=""></div>
+      <div class="item"><img src="https://archipro.co.nz/assets/photos/Otago-Polytechnic-Forth-Street-Mason--Wales-Architects-16204__ListingLargeW10.JPG?1" alt=""></div>
+  </div>
 </div>
     
 <script>
