58d9325cfb1453bc42406ac74cb3e8c4998e60ca
Anthony Legg <leggtc1@student.op.ac.nz>
2021-05-10 13:19:07 +1200

    Update made to the html structure of the Student Notes page/form
    this is to make the structure more consistent with other forms

--- routes/web.php
+++ routes/web.php
@@ -5,7 +5,7 @@ use App\Http\Controllers\PagesController;
 use App\Http\Controllers\EvidenceUploadController;
 
 Route::get('/', [PagesController::class, 'homepage'])->name('pages.index');
-Route::get('/cohortAdmin', [PagesController::class , 'cohortpage'])->name('pages.cohort');
+//Route::get('/cohortAdmin', [PagesController::class , 'cohortpage'])->name('pages.cohort');
 Route::get('/notes',[PagesController::class ,'notes'])->name('pages.notes');
 Route::get('/students',[PagesController::class ,'students'])->name('pages.students');
 Route::resource('uploads', EvidenceUploadController::class);
