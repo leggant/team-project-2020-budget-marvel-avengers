728f1cbd3c515d5ea069a471689a2465e058bcb1
Anthony Legg <leggtc1@student.op.ac.nz>
2021-05-14 09:00:02 +1200

    Fixes added as per code review for pull request #136
    Removed orhan div tags from html structure

--- resources/views/pages/notes/Notes_edit.blade.php
+++ resources/views/pages/notes/Notes_edit.blade.php
@@ -6,25 +6,26 @@ Studio Management Portal | Notes Edit
 
 @section('content')
 <section>
-  <h1>Student Notes</h1>
-  <form action="{{ route('notes.update', $note->id)}}" method="POST">
-  @csrf
-  @method('PUT')
-    <div class="form-col">
-      <div class="input-group">
-        <label for="student_name">Student Name:</label>
-        <input id="student_name" name="student_name" type="text" spellcheck="false" maxlength="255" value="{{ $note->student_name }}"> 
-      </div>
-    </div>
-    <div class="form-col">
-      <div class="input-group">
-        <label for="note">Notes:</label>
-        <textarea id="note" name="note" spellcheck="true">{{$note->note}}</textarea>
-      </div>
-        <input id="saveForm" name="saveForm" type="submit" value="Save" class="submit-btn">
-      </div>
-    </div>
-  </form>
-  
+    <h1>Student Notes</h1>
+    <form action="{{ route('notes.update', $note->id) }}" method="POST">
+        @csrf
+        @method('PUT')
+        <div class="form-col">
+            <div class="input-group">
+                <label for="student_name">Student Name:</label>
+                <input id="student_name" name="student_name" type="text" spellcheck="false" maxlength="255"
+                    value="{{ $note->student_name }}">
+            </div>
+        </div>
+        <div class="form-col">
+            <div class="input-group">
+                <label for="note">Notes:</label>
+                <textarea id="note" name="note" spellcheck="true">{{ $note->note }}</textarea>
+            </div>
+            <div class="input-group">
+                <input id="saveForm" name="saveForm" type="submit" value="Save" class="submit-btn">
+            </div>
+        </div>
+    </form>
 </section>
 @endsection
