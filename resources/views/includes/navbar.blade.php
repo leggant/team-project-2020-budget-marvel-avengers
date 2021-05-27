05664e68c58a1c84c036eb7267cc8abccf8168c6
Anthony Legg <61127354+leggant@users.noreply.github.com>
2021-05-27 13:46:06 +1200

    Update resources/views/includes/navbar.blade.php

--- resources/views/includes/navbar.blade.php
+++ resources/views/includes/navbar.blade.php
@@ -21,7 +21,7 @@
             @endguest
             @auth
                 <li class="nav-item dropdown">
-                    <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown"
+                    <a id="navbarDropdown" class="nav-link" href="#"
                         aria-haspopup="true" aria-expanded="false" v-pre>
                         {{ Auth::user()->name }}
                     </a>
