b566eae312a58cafddffe934de4e3b6bc7833f61
Anthony Legg <leggtc1@student.op.ac.nz>
2021-06-02 11:03:01 +1200

    Further updates to sass/css in cohorts. Still debugging some dispaly issues

--- resources/views/pages/cohort/cohort.blade.php
+++ resources/views/pages/cohort/cohort.blade.php
@@ -10,54 +10,62 @@
         </header>
         <form action="{{ route('cohort.store') }}" method="POST" class="cohort-form">
             @csrf
-                <div class="input-group">
-                    <label for="semester1">Semester 1
+            <div class="input-group">
+                <label for="semester1">Semester 1
                     <input type="radio" name="semester" id="semester1" value="Semester 1" checked></label>
+            </div>
+            <div class="input-group">
+                <label for="semester2">Semester 2
+                    <input type="radio" name="semester" id="semester2" value="Semester 2"></label>
+            </div>
+            <select id="student_name" name="year" required class="cohort-select">
+                <option value="2020">2020</option>
+                <option value="2021">2021</option>
+                <option value="2022">2022</option>
+                <option value="2023">2023</option>
+            </select>
+            <input id="saveForm" name="" type="submit" value="Submit" class="submit-btn">
+        </form>
+        <header>
+            <h1>Studio Paper Cohorts</h1>
+        </header>
+        <form action="{{ route('semesters.store') }}" method="POST" class="cohorts-form">
+            @csrf
+            <div class="form-col">
+                <div class="input-group">
+                    <label for="studio1">Studio 1
+                        <input type="radio" name="studio" id="studio1" value="Studio 1" checked>
+                    </label>
                 </div>
                 <div class="input-group">
-                    <label for="semester2">Semester 2
-                    <input type="radio" name="semester" id="semester2" value="Semester 2"></label>
-                </div>
-                <select id="student_name" name="year" required class="cohort-select">
-                    <option value="2020">2020</option>
-                    <option value="2021">2021</option>
-                    <option value="2022">2022</option>
-                    <option value="2023">2023</option>
-                </select>
-                <input id="saveForm" name="" type="submit" value="Submit" class="submit-btn">
-        </form>
-        <table>
-            <tr>
-                <form action="{{ route('semesters.store') }}" method="POST">
-                    <h1>Studio Paper Cohorts</h1>
-                    @csrf
-                    <div class="form-col">
-                        <label for="studio1">Studio 1</label>
-                        <input type="radio" name="studio" id="studio1" value="Studio 1" checked>
-                        <label for="studio2">Studio 2</label>
+                    <label for="studio2">Studio 2
                         <input type="radio" name="studio" id="studio2" value="Studio 2">
-                        <label for="studio3">Studio 3</label>
+                    </label>
+                </div>
+                <div class="input-group">
+                    <label for="studio3">Studio 3
                         <input type="radio" name="studio" id="studio3" value="Studio 3">
-                        <label for="studio4">Studio 4</label>
+                    </label>
+                </div>
+                <div class="input-group">
+                    <label for="studio4">Studio 4
                         <input type="radio" name="studio" id="studio4" value="Studio 4">
-                    </div>
-                    <div class="input-group">
-                        <select id="student_name" name="cohort_id" required>
-                            <option>--- Select Cohort ---</option>
-                            @foreach ($cohort as $value)
-                                <option value="{{ $value->id }}">{{ $value->year }}</option>
-                            @endforeach
-                        </select>
-                        <textarea id="note2" name="students" placeholder="Enter students name ✍🏻"></textarea>
-                    </div>
-                    <div class="input-group">
-                        <input id="saveForm" name="saveForm" type="submit" value="Submit" class="submit-btn">
-                    </div>
-                </form>
-            </tr>
-        </table>
+                    </label>
+                </div>
+            </div>
+            <div>
+                <select id="student_name" name="cohort_id" required>
+                    <option>--- Select Cohort ---</option>
+                    @foreach ($cohort as $value)
+                        <option value="{{ $value->id }}">{{ $value->year }}</option>
+                    @endforeach
+                </select>
+                <textarea id="student-names" name="students" placeholder="Enter students name ✍🏻"></textarea>
+            </div>
+            <input id="saveForm" name="saveForm" type="submit" value="Submit" class="submit-btn">
+        </form>
         <table cellpadding="0" cellspacing="0" cellborder="0">
-            <thead class="">
+            <thead>
                 <tr>
                     <th>List of Cohorts 📜</th>
                 </tr>
