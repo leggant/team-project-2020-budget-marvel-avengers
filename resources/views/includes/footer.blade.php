03591ba466d91954d917c2ddc540e53c392196ae
Mohammed-alhasan <chookepsn@gmail.com>
2021-06-07 16:51:40 +1200

    remove white spaces and unused css code

--- resources/views/includes/footer.blade.php
+++ resources/views/includes/footer.blade.php
@@ -35,43 +35,37 @@
         <div class="col">
             <h1> Auckland Campus </h1>
             <address>
-              <p>
-                  Level 1,350 Queen Street,<br>
-                  PO BOX 5268,<br>
-                  Auckland 1141,<br>
-                  New Zealand<br>
-              </p>
-          </address>
-          <p><br>Free-Phone (NZ Only): <a href="tel:+640800111212">0800 111 212</a></p>
-          <p>International: <a href="tel:+64093090301">+64 (09) 309-0301</a></p>
-          <p>Email: <a href="mailto:auckland@op.ac.nz">auckland@op.ac.nz</a></p>
+                <p>
+                    Level 1,350 Queen Street,<br>
+                    PO BOX 5268,<br>
+                    Auckland 1141,<br>
+                    New Zealand<br>
+                </p>
+            </address>
+            <p><br>Free-Phone (NZ Only): <a href="tel:+640800111212">0800 111 212</a></p>
+            <p>International: <a href="tel:+64093090301">+64 (09) 309-0301</a></p>
+            <p>Email: <a href="mailto:auckland@op.ac.nz">auckland@op.ac.nz</a></p>
         </div>
     </div>
- <div class="col social">
-       <h1>Social</h1>
-    
-    <div class="wrapper">
-
-        <div class="icon facebook">
-          <div class="tooltip">Facebook</div><a href="https://www.facebook.com/OtagoPoly">
-          <span><i class="fab fa-facebook-f"></i></span>
-        </div></a>
-
-    
-        <div class="icon instagram">
-          <div class="tooltip">Instagram</div><a href="https://www.instagram.com/otagopolytechnic/">
-          <span><i class="fab fa-instagram"></i></span></a>
+    <div class="col social">
+        <h1>Social</h1>
+        <div class="wrapper">
+            <div class="icon facebook">
+                <div class="tooltip">Facebook</div><a href="https://www.facebook.com/OtagoPoly">
+                    <span><i class="fab fa-facebook-f"></i></span>
+            </div></a>
+            <div class="icon instagram">
+                <div class="tooltip">Instagram</div><a href="https://www.instagram.com/otagopolytechnic/">
+                    <span><i class="fab fa-instagram"></i></span></a>
+            </div>
+            <div class="icon LinkedIn">
+                <div class="tooltip">LinkedIn</div><a href="https://www.linkedin.com/school/otagopolytechnic/">
+                    <span> <i class="fab fa-linkedin"></i></i></span></a>
+            </div>
+            <div class="icon youtube">
+                <div class="tooltip">Youtube</div><a href="https://www.youtube.com/channel/UCRSet54nNLAiFTGvVT7b0-g">
+                    <span><i class="fab fa-youtube"></i></span></a>
+            </div>
         </div>
-
-        <div class="icon LinkedIn">
-          <div class="tooltip">LinkedIn</div><a href="https://www.linkedin.com/school/otagopolytechnic/">
-          <span> <i class="fab fa-linkedin"></i></i></span></a>
-        </div>
-       
-        <div class="icon youtube">
-          <div class="tooltip">Youtube</div><a href="https://www.youtube.com/channel/UCRSet54nNLAiFTGvVT7b0-g">
-          <span><i class="fab fa-youtube"></i></span></a>
-        </div>
-      </div>
     </div>
     <!-- END OF FOOTER -->
