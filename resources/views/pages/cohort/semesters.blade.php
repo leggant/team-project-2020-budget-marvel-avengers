1287a31070ae77f0aea348136af57bdd1c109bb8
Conor <westcl4@student.op.ac.nz>
2021-06-09 22:11:34 +1200

    Added student last name variable to evidence list

--- resources/views/pages/cohort/semesters.blade.php
+++ resources/views/pages/cohort/semesters.blade.php
@@ -8,7 +8,7 @@
         <h1>Students in  {{$cohort->semester}} {{$cohort->year}}</h1>
         @foreach ($cohort->semesters as $value)
         <h1>{{$value->studio}}</h1>
-            <p>{{ $value->students }}</p>
+            <p>{{ $value->students }},</p>
             {{-- <a href="{{ route('cohort.edit',$value->id) }}"><button class="btn btn-edit">Edit</button>  --}}
             
         @endforeach
